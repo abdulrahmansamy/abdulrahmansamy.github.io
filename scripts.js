56592389415e85577ee3c4156bb48a65b3e2ee6f
abdulrahmansamy <abdul.m.samy@gmail.com>
2025-02-16 16:53:14 +0300

    fix contact me form

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
       data[key] = value;
     });
   
-    console.log('Sending data:', data);
+    // console.log('Sending data:', data);
   
     fetch('https://us-central1-anthos-demo-project-0001.cloudfunctions.net/node-mail-function/send-email', {
       method: 'POST',
@@ -17,20 +17,21 @@ document.getElementById('contact-form').addEventListener('submit', function(even
       body: JSON.stringify(data)
     })
     .then(response => {
-      console.log('Response:', response);
+      // console.log('Response:', response); 
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
       }
       return response.json();
     })
     .then(result => {
-      console.log('Result:', result);
+      // console.log('Result:', result);
       if (result.success) {
         alert('Email sent successfully!');
       } else {
         alert('Failed to send email.');
       }
     })
+    
     .catch(error => {
       console.error('There was a problem with the fetch operation:', error);
       alert('Failed to send email. Error: ' + error.message);
